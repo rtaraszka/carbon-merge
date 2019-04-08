9602fa4fc6f2858c47cd91a1fa7e0b5ef5c04b56
AdamRudnicki96 <adam.rudnicki@uxpin.com>
2019-04-08 16:10:06 +0200

    Handle OverflowMenu component

--- src/OverflowMenu/presets/0-default.jsx
+++ src/OverflowMenu/presets/0-default.jsx
@@ -1,17 +1,41 @@
-import * as React from "react";
+import React from "react";
 import OverflowMenu from "../OverflowMenu";
 import OverflowMenuItem from "../../OverflowMenuItem/OverflowMenuItem";
 
+const overflowMenuItemProps = {
+    className: 'some-class',
+};
+
 export default (
-  <OverflowMenu uxpId="1">
-    <OverflowMenuItem uxpId="2" itemText="Option 1" primaryFocus={true} />
-    <OverflowMenuItem
-      uxpId="3"
-      itemText="Option 2 is an example of a really long string and how we recommend handling this"
-      requireTitle
-    />
-    <OverflowMenuItem uxpId="4" itemText="Option 3" />
-    <OverflowMenuItem uxpId="5" itemText="Option 4" />
-    <OverflowMenuItem uxpId="6" itemText="Danger option" hasDivider isDelete />
-  </OverflowMenu>
+    <OverflowMenu
+        floatingMenu
+        direction="bottom"
+        uxpId="OverflowMenu0">
+        <OverflowMenuItem
+            {...overflowMenuItemProps}
+            itemText="Option 1"
+            primaryFocus
+            uxpId="OverflowMenuItem0"
+        />
+        <OverflowMenuItem
+            {...overflowMenuItemProps}
+            itemText="Option 2 is an example of a really long string and how we recommend handling this"
+            requireTitle
+            uxpId="OverflowMenuItem1"
+        />
+        <OverflowMenuItem {...overflowMenuItemProps} itemText="Option 3" uxpId="OverflowMenuItem2" />
+        <OverflowMenuItem {...overflowMenuItemProps} itemText="Option 4" uxpId="OverflowMenuItem3" />
+        <OverflowMenuItem
+            {...overflowMenuItemProps}
+            itemText="Option 5"
+            uxpId="OverflowMenuItem4"
+        />
+        <OverflowMenuItem
+            {...overflowMenuItemProps}
+            itemText="Danger option"
+            hasDivider
+            isDelete
+            uxpId="OverflowMenuItem5"
+        />
+    </OverflowMenu>
 );
