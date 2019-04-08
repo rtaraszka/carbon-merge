5d243235117a1dcc41fdc57f579a06e7c9b458a2
AdamRudnicki96 <adam.rudnicki@uxpin.com>
2019-04-08 15:38:24 +0200

    Handle Modal component

---
+++ src/Modal/presets/0-default.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Modal from "../Modal";
+
+export default (
+    <Modal
+        className="some-class"
+        open
+        modalHeading="Modal heading"
+        modalLabel="Modal label"
+        primaryButtonText="Primary Button"
+        secondaryButtonText="Secondary Button"
+        selectorPrimaryFocus="[data-modal-primary-focus]"
+        iconDescription="Close the modal"
+        uxpId="Modal0">
+        Please see ModalWrapper for more examples and demo of the functionality.
+    </Modal>
+);
