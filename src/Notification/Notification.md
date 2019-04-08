883298953d90beeb67b5a36de990129a892753d5
AdamRudnicki96 <adam.rudnicki@uxpin.com>
2019-04-08 15:39:27 +0200

    Remove Notification component

--- src/Notification/Notification.md
+++
@@ -1,5 +0,0 @@
-# Notification
-
-```javascript
-<Notification uxpId="1" kind="success" type="inline" title="Merge is a revolution!" subtitle="UXPin Merge ❤️ IBM Carbon"/>
-```
--- src/Notification/presets/0-default.jsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import Notification from '../Notification';
-
-export default (
-<Notification uxpId="1" kind="success" type="inline" title="Merge is a revolution!" subtitle="UXPin Merge ❤️ IBM Carbon"/>
-);
