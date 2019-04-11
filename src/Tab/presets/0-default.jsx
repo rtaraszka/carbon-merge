a709e5b2e2098913190b63ab548c12ec8d887735
AdamRudnicki96 <adam.rudnicki@uxpin.com>
2019-04-11 15:01:09 +0200

    Handle Tabs component

--- src/Tab/presets/0-default.jsx
+++ src/Tab/presets/0-default.jsx
@@ -1,7 +1,9 @@
-import * as React from 'react';
+import React from 'react';
 import Tab from '../Tab';
 
 export default (
-      <Tab uxpId="1">Tab 1</Tab>
+    <Tab href="#" role="presentation" tabIndex={0} label="Label" uxpId="Tab0">
+        Content for second tab goes here.
+    </Tab>
 );
 
